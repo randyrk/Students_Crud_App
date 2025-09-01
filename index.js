ff7c296bfa1128b8ea4cda5861eed80dedb045d9
santhosh <santhoshvs893@gmail.com>
2025-09-01 17:18:57 +0530

    start comment updated

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/students", studentRoutes);
 
 
+
 //connecting to server
 const uri = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3000;
